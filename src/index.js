3775cbfadd37ce8f9d84a8a5fe73a23c98ef5625
Oleksandr Tsoy <60102210+js1212abcd@users.noreply.github.com>
2020-08-17 16:56:01 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,106 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import * as Redux from 'redux';
+import * as ReactRedux from 'react-redux';
+import './index.css';
+import AuthorQuiz from './AuthorQuiz';
+import AddAuthorForm from './AddAuthorForm';
+import registerServiceWorker from './registerServiceWorker';
+import { shuffle, sample } from 'underscore';
+
+const authors = [
+    {
+        name: 'Mark Twain',
+        imageUrl: 'images/authors/mark-twain.jpg',
+        imageSource: 'http://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Mark_Twain%2C_Brady-Handy_photo_portrait%2C_Feb_7%2C_1871%2C_cropped.jpg/400px-Mark_Twain%2C_Brady-Handy_photo_portrait%2C_Feb_7%2C_1871%2C_cropped.jpg',
+        books: ['The Adventures of Huckleberry Finn']
+    },
+    {
+        name: 'Joseph Conrad',
+        imageUrl: 'images/authors/joseph-conrad.png',
+        imageSource: 'http://upload.wikimedia.org/wikipedia/commons/thumb/0/07/Joseph_Conrad.PNG/350px-Joseph_Conrad.PNG',
+        books: ['Heart of Darkness']
+    },
+    {
+        name: 'J.K. Rowling',
+        imageUrl: 'images/authors/jk-rowling.jpg',
+        imageSource: 'http://upload.wikimedia.org/wikipedia/commons/thumb/5/5d/J._K._Rowling_2010.jpg/440px-J._K._Rowling_2010.jpg',
+        books: ['Harry Potter and the Sorcerers Stone']
+    },
+    {
+        name: 'Stephen King',
+        imageUrl: 'images/authors/stephen-king.jpg',
+        imageSource: 'http://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Stephen_King%2C_Comicon.jpg/440px-Stephen_King%2C_Comicon.jpg',
+        books: ['The Shining', 'IT']
+    },
+    {
+        name: 'Charles Dickens',
+        imageUrl: 'images/authors/charles-dickens.jpg',
+        imageSource: 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/aa/Dickens_Gurney_head.jpg/440px-Dickens_Gurney_head.jpg',
+        books: ['David Copperfield', 'A Tale of Two Cities']
+    },
+    {
+        name: 'William Shakespeare',
+        imageUrl: 'images/authors/william-shakespeare.jpg',
+        imageSource: 'http://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Shakespeare.jpg/500px-Shakespeare.jpg',
+        books: ['Hamlet', 'Macbeth', 'Romeo and Juliet']
+    }
+];
+
+let store = Redux.createStore(reducer);
+
+function reducer(state = { authors, turnData: getTurnData(authors), highlight: '' }, action) {
+    switch (action.type) {
+        case 'ANSWER_SELECTED':
+            const isCorrect = state.turnData.author.books.some((book) => book === action.answer);
+            return Object.assign(
+                {},
+                state,
+                {
+                    highlight: isCorrect ? 'correct' : 'wrong'
+                });
+        case 'CONTINUE':
+            return Object.assign(
+                {},
+                state,
+                {
+                    highlight: '',
+                    turnData: getTurnData(state.authors)
+                });
+        case 'ADD_AUTHOR':
+            return Object.assign({}, state, { authors: state.authors.concat(action.author) });
+        default: return state;
+    }
+}
+
+function getTurnData(authors) {
+    const allBooks = authors.reduce(function (p, c, i) {
+        return p.concat(c.books);
+    }, []);
+
+    const fourRandomBooks = shuffle(allBooks).slice(0, 4);
+    const answer = sample(fourRandomBooks);
+
+    return {
+        books: fourRandomBooks,
+        author: authors.find((author) =>
+            author.books.some((title) =>
+                title === answer))
+    };
+}
+
+ReactDOM.render(
+    <BrowserRouter>
+        <ReactRedux.Provider store={store}>
+            <React.Fragment>
+                <Route exact path='/' component={AuthorQuiz} />
+                <Route path='/add' component={AddAuthorForm} />
+            </React.Fragment>
+        </ReactRedux.Provider>
+    </BrowserRouter>
+    , document.getElementById('root'));
+
+export default registerServiceWorker();
+
+  
